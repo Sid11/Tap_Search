a25488fbf3b17d8b6dd4a75d8d74f82ee8200f14
Siddhesh Jadhav <jsiddhesh96@gmail.com>
2019-12-01 19:17:23 +0530

    make it better

--- TapSearch/settings.py
+++ TapSearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^vv9=&aj-ql#fezve89hl74a@1%tvcpl-d75ux188g!_pkbbdc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://tapsearchwork.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['tapsearchwork.herokuapp.com']
 
 
 # Application definition
